7e64ec3f94d5c5a34bbb371254e84ceffd7472fb
DIEMHUONG\DIEMHUONG <tamndt1994@gmail.com>
2018-05-26 19:26:22 +0700

    tam

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
-import { Card, Icon, Image, Button } from 'semantic-ui-react';
+import { Card, Icon, Image, Button, Grid, Menu } from 'semantic-ui-react';
 import Layout from '../components/Layout';
-import {Link} from '../routes';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
 
     static async getInitialProps() {
         // const campaigns = await factory.methods.getDeployedCampaigns().call();
-        // return { campaigns };
+        const campaigns = [];
+        return { campaigns };
     }
 
     renderCampaigns() {
@@ -27,13 +28,31 @@ class CampaignIndex extends Component {
         return <Card.Group items={items} />
     }
 
+    state = { activeItem: 'All' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    renderMenu() {
+        const Menus = [
+            { key: 'All'   ,name: 'All' },
+            { key: 'Games' ,name: 'Games' },
+            { key: 'Art'   ,name: 'Art' },
+            { key: 'Film'  ,name: 'Film' },
+            { key: 'Techn' ,name: 'Technology' },
+            { key: 'Vehic' ,name: 'Vehicle' },
+        ];
+
+        return (
+            <Menu tabular defaultActiveIndex='0' items={Menus} onItemClick={this.handleItemClick}/>
+        );
+    }
+
     render() {
         return (
             <Layout>
-                <div>
-                    <h3>Opening Projects</h3>
-                    {this.renderCampaigns()}
-                </div>
+                {this.renderMenu()}
+                <h3>{this.state.activeItem}</h3>
+                {this.renderCampaigns()}
             </Layout>
         )
     }
